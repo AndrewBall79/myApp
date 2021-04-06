f656fdb8d4e6aacb8356a1753a12f36693b899d8
AndrewBall79 <andy.bob.ball@gmail.com>
2021-04-05 20:50:00 -0500

    date time

--- jokes.py
+++ jokes.py
@@ -1,19 +1,22 @@
 import pyjokes
-from collections import Counter, defaultdict, OrderedDict
+import datetime
+# from collections import Counter, defaultdict, OrderedDict
 
 print(pyjokes.get_joke())
 
-li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# li = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# print(Counter(li))
+#
+# dictionary = defaultdict(lambda: 5, {"a": 1, "b": 2})
+# print(dictionary['c'])
+#
+# d = OrderedDict()
+# d['a'] = 1
+# d['b'] = 2
+#
+# d2 = OrderedDict()
+# d2['a'] = 1
+# d2['b'] = 2
 
-print(Counter(li))
-
-dictionary = defaultdict(lambda: 5, {"a": 1, "b": 2})
-print(dictionary['c'])
-
-d = OrderedDict()
-d['a'] = 1
-d['b'] = 2
-
-d2 = OrderedDict()
-d2['a'] = 1
-d2['b'] = 2
+print(datetime.date.today())
