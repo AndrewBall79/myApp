bca493f922149764ff99a27b474655d07a0b79bf
AndrewBall79 <andy.bob.ball@gmail.com>
2021-04-06 22:19:05 -0500

    translations

--- file_reader.py
+++ file_reader.py
@@ -1,11 +1,19 @@
+from translate import Translator
+
+translator = Translator(to_lang='fr')
 try:
-    with open('test.txt', 'a') as my_file:
-        print(my_file.write(""))
+    with open('testo.txt', 'r') as my_file:
+        text = my_file.read()
+        print(translator.translate(text))
+        with open('text_fr_translate.txt', 'a') as my_file2:
+            print(my_file2)
 except FileNotFoundError as err:
     print('Filo no existo')
     raise err
 except IOError as err:
     print('IO error')
     raise err
+
+
 
 
