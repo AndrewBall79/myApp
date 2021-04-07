8dbe048f299452244a24e7e48041d457974a1d33
AndrewBall79 <andy.bob.ball@gmail.com>
2021-04-07 17:06:12 -0500

    finalizing

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import unittest
 import main
 
+
 class TestMain(unittest.TestCase):
+    def setUp(self) -> None:
+        print('A')
+
     def test_do_stuff(self):
         test_param = 10
         result = main.do_stuff(test_param)
@@ -21,3 +25,10 @@ class TestMain(unittest.TestCase):
         test_param = ''
         result = main.do_stuff(test_param)
         self.assertEqual(result, "Please Enter Number")
+
+    def tearDown(self) -> None:
+        print('C')
+
+
+if __name__ == '__main__':
+    unittest.main()
