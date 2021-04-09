75cc7ab45e2cc7ef2da1e73061a6fc42ce2c1fd8
AndrewBall79 <andy.bob.ball@gmail.com>
2021-04-08 19:21:57 -0500

    img process

--- images.py
+++ images.py
@@ -1,6 +1,9 @@
 from PIL import Image, ImageFilter
 
 
-img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.filter(ImageFilter.BLUR)
-filtered_img.save('blur.png', 'png')
+img = Image.open('./Pokedex/astro.jpg')
+img.thumbnail((400, 400))
+img.save('./Pokedex/thumbnail.jpg')
+# filtered_img = img.filter(ImageFilter.BLUR)
+# bw_img = img.convert('L')
+# bw_img.save('bw.png', 'png')
